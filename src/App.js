fa6c104708549eb06e3b582879fc454d9f436f2b
Yolea Mayers <yoleamayers@Yoleas-Air.fios-router.home>
2020-03-31 14:49:51 -0400

    the countries are finally rendered

--- src/App.js
+++ src/App.js
@@ -9,34 +9,24 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      response: ""
+      countries: []
     };
   }
   componentDidMount = async () => {
     const response = await axios.get(`https://api.covid19api.com/summary`);
 
-    this.setState({ response: response.data.Countries });
+    this.setState({ countries: response.data.Countries });
   };
 
   render() {
-    // used object.keys because I kept getting an error that .map is not a function
-    //first map through the response Countries - returns only indices - not names
-    let countryDisp = Object.keys(this.state.response).map((value, idx) => {
-      return (
-        //add } back here
-        <div className="country" key={idx}>
-          {value}
-        </div>
-      );
-    });
-
     return (
       <div>
         <main>
           <Header />
-
           <div className="countries">
-            <Home className="country" country={countryDisp} />
+            {this.state.countries.map((countries, idx) => (
+              <Home className="country" key={idx} country={countries.Country} />
+            ))}
           </div>
         </main>
       </div>
@@ -44,8 +34,3 @@ class App extends React.Component {
   }
 }
 export default App;
-// return (<div className="container"> {this.state.response.data.Countries.map}((value, idx) => <p key={idx}>{value.map}</p>((country) => {<Home className="country" key={idx}>{country}</Home>}
-
-// )
-// );
-// };
