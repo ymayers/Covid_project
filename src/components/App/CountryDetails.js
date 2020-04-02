9105f38d43d13339f58d8e25db47ef193b390add
Yolea Mayers <yoleamayers@Yoleas-Air.fios-router.home>
2020-04-02 16:09:32 -0400

    preparing for netlify

--- src/components/App/CountryDetails.js
+++ src/components/App/CountryDetails.js
@@ -1,49 +1,40 @@
 import React, { Component } from "react";
 
-
 //displays number of covid-19 cases on separate page
-//in variable nation - idx is the country name in the url
-//it is being matched to the prop with same country name using the .find method
+
 
 //need to build this out
 //would like to use several storybook components here
-//trying to access json data to obtain country code to display flag
-//I may have to map through name to get the country code
 
 class CountryDetails extends Component {
   constructor() {
     super();
-  // this.state= {image:""}
   }
-//to get image by countryCode
-  // loadImage = countryCode => {
-  //   import(`../../data/flags-large/${countryCode}.png`).then(image => {
-  //     this.setState({
-  //       image
-  //     });
-  //   });
-  // };
-
-//to map for countryCode - gives access to name json file objects - hopefully
-// this.name.map(countryName => nation === countryName.name)
-
-
 
   render() {
+    //variable nation - idx is the country name in the url
+    //it is being matched to the prop with same country name using the .find method
     const nation = this.props.match.params.idx;
     const countryInfo = this.props.cases.find(item => nation === item.Country);
-    const countryCode = this.props.image.find(name => nation === name.name)
-    console.log(countryCode.alpha2)
+    //to find countryCode - giving access to json file objects
+    const countryCode = this.props.image.find(name => nation === name.name);
+    //to get image by countryCode
+    const images = require.context(`../../data/flags-large`, true);
+    let img = images(`./${countryCode.alpha2}.png`);
 
     return (
       <div className="details-container">
         <div className="details-name">
-          <h1>Country Name</h1>
-          <p>{countryInfo.Country}</p>
+          <img src={img} alt="flag" />
+          <h1>{countryInfo.Country}</h1>
         </div>
         <div className="details-confirmed">
-          <h1>Number of Confirmed COVID-19 Cases</h1>
+          <h2>Number of Confirmed COVID-19 Cases</h2>
           <p>{countryInfo.TotalConfirmed}</p>
+          <h2>Total Recovered</h2>
+          <p>{countryInfo.TotalRecovered}</p>
+          <h2>Total Deaths</h2>
+          <p>{countryInfo.TotalDeaths}</p>
         </div>
       </div>
     );
